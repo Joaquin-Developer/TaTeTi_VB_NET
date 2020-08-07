ae7060f02f5e958db41f91cf129ee44aed669714
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-07 15:57:19 -0300

    first commit

---
+++ Ejerc3_TaTeTi/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lbl_0_0 = New System.Windows.Forms.Label()
+        Me.lbl_1_2 = New System.Windows.Forms.Label()
+        Me.lbl_1_1 = New System.Windows.Forms.Label()
+        Me.lbl_1_0 = New System.Windows.Forms.Label()
+        Me.lbl_0_2 = New System.Windows.Forms.Label()
+        Me.lbl_0_1 = New System.Windows.Forms.Label()
+        Me.lbl_2_2 = New System.Windows.Forms.Label()
+        Me.lbl_2_0 = New System.Windows.Forms.Label()
+        Me.lbl_2_1 = New System.Windows.Forms.Label()
+        Me.lblTurnoJug = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(23, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(423, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(145, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 320)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(297, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 320)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(20, 211)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(426, 23)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'lbl_0_0
+        '
+        Me.lbl_0_0.AutoSize = True
+        Me.lbl_0_0.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_0_0.Location = New System.Drawing.Point(49, 26)
+        Me.lbl_0_0.Name = "lbl_0_0"
+        Me.lbl_0_0.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_0_0.TabIndex = 4
+        Me.lbl_0_0.Text = "X"
+        '
+        'lbl_1_2
+        '
+        Me.lbl_1_2.AutoSize = True
+        Me.lbl_1_2.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_1_2.Location = New System.Drawing.Point(353, 142)
+        Me.lbl_1_2.Name = "lbl_1_2"
+        Me.lbl_1_2.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_1_2.TabIndex = 5
+        Me.lbl_1_2.Text = "X"
+        '
+        'lbl_1_1
+        '
+        Me.lbl_1_1.AutoSize = True
+        Me.lbl_1_1.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_1_1.Location = New System.Drawing.Point(206, 142)
+        Me.lbl_1_1.Name = "lbl_1_1"
+        Me.lbl_1_1.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_1_1.TabIndex = 6
+        Me.lbl_1_1.Text = "X"
+        '
+        'lbl_1_0
+        '
+        Me.lbl_1_0.AutoSize = True
+        Me.lbl_1_0.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_1_0.Location = New System.Drawing.Point(49, 142)
+        Me.lbl_1_0.Name = "lbl_1_0"
+        Me.lbl_1_0.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_1_0.TabIndex = 7
+        Me.lbl_1_0.Text = "X"
+        '
+        'lbl_0_2
+        '
+        Me.lbl_0_2.AutoSize = True
+        Me.lbl_0_2.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_0_2.Location = New System.Drawing.Point(353, 26)
+        Me.lbl_0_2.Name = "lbl_0_2"
+        Me.lbl_0_2.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_0_2.TabIndex = 8
+        Me.lbl_0_2.Text = "X"
+        '
+        'lbl_0_1
+        '
+        Me.lbl_0_1.AutoSize = True
+        Me.lbl_0_1.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_0_1.Location = New System.Drawing.Point(206, 26)
+        Me.lbl_0_1.Name = "lbl_0_1"
+        Me.lbl_0_1.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_0_1.TabIndex = 9
+        Me.lbl_0_1.Text = "X"
+        '
+        'lbl_2_2
+        '
+        Me.lbl_2_2.AutoSize = True
+        Me.lbl_2_2.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_2_2.Location = New System.Drawing.Point(353, 246)
+        Me.lbl_2_2.Name = "lbl_2_2"
+        Me.lbl_2_2.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_2_2.TabIndex = 10
+        Me.lbl_2_2.Text = "X"
+        '
+        'lbl_2_0
+        '
+        Me.lbl_2_0.AutoSize = True
+        Me.lbl_2_0.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_2_0.Location = New System.Drawing.Point(49, 246)
+        Me.lbl_2_0.Name = "lbl_2_0"
+        Me.lbl_2_0.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_2_0.TabIndex = 11
+        Me.lbl_2_0.Text = "X"
+        '
+        'lbl_2_1
+        '
+        Me.lbl_2_1.AutoSize = True
+        Me.lbl_2_1.Font = New System.Drawing.Font("Microsoft Sans Serif", 45.0!)
+        Me.lbl_2_1.Location = New System.Drawing.Point(206, 246)
+        Me.lbl_2_1.Name = "lbl_2_1"
+        Me.lbl_2_1.Size = New System.Drawing.Size(70, 69)
+        Me.lbl_2_1.TabIndex = 12
+        Me.lbl_2_1.Text = "X"
+        '
+        'lblTurnoJug
+        '
+        Me.lblTurnoJug.AutoSize = True
+        Me.lblTurnoJug.Location = New System.Drawing.Point(33, 356)
+        Me.lblTurnoJug.Name = "lblTurnoJug"
+        Me.lblTurnoJug.Size = New System.Drawing.Size(89, 13)
+        Me.lblTurnoJug.TabIndex = 13
+        Me.lblTurnoJug.Text = "Turno: Jugador X"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(493, 378)
+        Me.Controls.Add(Me.lblTurnoJug)
+        Me.Controls.Add(Me.lbl_2_1)
+        Me.Controls.Add(Me.lbl_2_0)
+        Me.Controls.Add(Me.lbl_2_2)
+        Me.Controls.Add(Me.lbl_0_1)
+        Me.Controls.Add(Me.lbl_0_2)
+        Me.Controls.Add(Me.lbl_1_0)
+        Me.Controls.Add(Me.lbl_1_1)
+        Me.Controls.Add(Me.lbl_1_2)
+        Me.Controls.Add(Me.lbl_0_0)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Ta Te Ti - El Juego"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lbl_0_0 As Label
+    Friend WithEvents lbl_1_2 As Label
+    Friend WithEvents lbl_1_1 As Label
+    Friend WithEvents lbl_1_0 As Label
+    Friend WithEvents lbl_0_2 As Label
+    Friend WithEvents lbl_0_1 As Label
+    Friend WithEvents lbl_2_2 As Label
+    Friend WithEvents lbl_2_0 As Label
+    Friend WithEvents lbl_2_1 As Label
+    Friend WithEvents lblTurnoJug As Label
+End Class
---
+++ Ejerc3_TaTeTi/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+
+    Dim jugador As String = "X"
+    Dim turno As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        limpiarTablero()
+    End Sub
+
+    Private Sub limpiarTablero()
+        ' dejamos un espacio para poder clickear el "caracter" en el Form
+        lbl_0_0.Text = " "
+        lbl_0_1.Text = " "
+        lbl_0_2.Text = " "
+        lbl_1_0.Text = " "
+        lbl_1_1.Text = " "
+        lbl_1_2.Text = " "
+        lbl_2_0.Text = " "
+        lbl_2_1.Text = " "
+        lbl_2_2.Text = " "
+
+    End Sub
+
+    Private Sub lbl_0_0_Click(sender As Object, e As EventArgs) Handles lbl_0_0.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_0_1_Click(sender As Object, e As EventArgs) Handles lbl_0_1.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_0_2_Click(sender As Object, e As EventArgs) Handles lbl_0_2.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_1_0_Click(sender As Object, e As EventArgs) Handles lbl_1_0.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_1_1_Click(sender As Object, e As EventArgs) Handles lbl_1_1.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_1_2_Click(sender As Object, e As EventArgs) Handles lbl_1_2.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_2_0_Click(sender As Object, e As EventArgs) Handles lbl_2_0.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_2_1_Click(sender As Object, e As EventArgs) Handles lbl_2_1.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub lbl_2_2_Click(sender As Object, e As EventArgs) Handles lbl_2_2.Click
+        jugada(sender)
+    End Sub
+
+    Private Sub jugada(ByVal sender As System.Object)
+        If sender.Text.Equals(" ") Then
+            sender.Text = jugador
+            turno += 1
+            If hayGanador() Then
+                If jugador.Equals("X") Then
+                    ' gana el jugador X
+                    MsgBox("GANA EL JUGADOR X ", vbInformation, "Final del juego!")
+                Else
+                    ' gana el jugador O
+                    MsgBox("GANA EL JUGADOR O ", vbInformation, "Final del juego!")
+
+                End If
+                finalJuego()
+
+            ElseIf turno = 9 Then
+                MsgBox("EMPATE!", vbEmpty, "Mensaje")
+                finalJuego()
+            End If
+
+            If jugador.Equals("X") Then
+                jugador = "O"
+                lblTurnoJug.Text = "Turno jugador: O"
+            Else
+                jugador = "X"
+                lblTurnoJug.Text = "Turno jugador: X"
+            End If
+
+        Else
+            ' caso que sender.Text distinto a null
+            MsgBox("Posición ocupada!", vbExclamation, "Mensaje")
+        End If
+
+    End Sub
+
+    Private Function hayGanador() As Boolean
+        ' determina si hay TaTeTi o no
+        'Dim campoVacio As String = " "
+        Dim x As Boolean
+
+        If (lbl_0_0.Text = lbl_1_0.Text) And (lbl_1_0.Text = lbl_2_0.Text) And lbl_0_0.Text <> " " Then
+            x = True
+        ElseIf (lbl_0_1.Text = lbl_1_1.Text And lbl_1_1.Text = lbl_2_1.Text) And lbl_0_1.Text <> " " Then
+            x = True
+        ElseIf (lbl_0_2.Text = lbl_1_2.Text And lbl_1_2.Text = lbl_2_2.Text) And lbl_0_2.Text <> " " Then
+            x = True
+        ElseIf (lbl_0_0.Text = lbl_0_1.Text And lbl_0_1.Text = lbl_0_2.Text) And lbl_0_0.Text <> " " Then
+            x = True
+        ElseIf (lbl_1_0.Text = lbl_1_1.Text And lbl_1_1.Text = lbl_1_2.Text) And lbl_1_0.Text <> " " Then
+            x = True
+        ElseIf (lbl_2_0.Text = lbl_2_1.Text And lbl_2_1.Text = lbl_2_2.Text) And lbl_2_0.Text <> " " Then
+            x = True
+        ElseIf (lbl_0_0.Text = lbl_1_1.Text And lbl_1_1.Text = lbl_2_2.Text) And lbl_0_0.Text <> " " Then
+            x = True
+        ElseIf (lbl_0_2.Text = lbl_1_1.Text And lbl_1_1.Text = lbl_2_0.Text) And lbl_0_2.Text <> " " Then
+            x = True
+        Else
+            x = False
+        End If
+
+        Return x
+
+    End Function
+
+    Private Sub finalJuego()
+
+        If MsgBox("Deseas jugar de nuevo?", vbInformation + vbYesNo, "Mensaje") = MsgBoxResult.Yes Then
+            Application.Restart() ' reinicio de aplicacion.-
+        Else
+            Application.Exit()
+        End If
+
+    End Sub
+
+End Class
